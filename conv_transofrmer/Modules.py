c480126a8199aeb3879c4af7c89c8a9e3a49b5c5
zhongyuan <zhongyuan12345@qq.com>
2021-01-22 17:53:57 +0800

    finish multihead convolutional transformer

--- conv_transofrmer/Modules.py
+++ conv_transofrmer/Modules.py
@@ -2,22 +2,53 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class ScaledDotProductAttention(nn.Module):
-    ''' Scaled Dot-Product Attention '''
 
-    def __init__(self, temperature, attn_dropout=0.1):
+class ConvolutionalAttention(nn.Module):
+    ''' Convolutional Attention '''
+
+    def __init__(self, d_feature=32, d_model=32, d_attention=1, attn_dropout=0.1):
         super().__init__()
-        self.temperature = temperature
+        self.d_feature = d_feature
+        self.d_model = d_model
+        self.d_attention = d_attention
+
+        self.conv_qs = nn.Conv2d(d_feature, d_model, kernel_size=1)
+        self.conv_ks = nn.Conv2d(d_feature, d_model, kernel_size=1)
+        self.conv_vs = nn.Conv2d(d_feature, d_model, kernel_size=1)
+        self.conv_attenion = nn.Conv2d(d_model, d_attention, kernel_size=5, padding=2)
         self.dropout = nn.Dropout(attn_dropout)
 
-    def forward(self, q, k, v, mask=None):
+    def forward(self, q, k, v):
+        # input size of: b x l x c x h x w
+        d_feature = self.d_feature
+        d_model = self.d_model
 
-        attn = torch.matmul(q / self.temperature, k.transpose(2, 3))
+        sz_b, len_q, len_k, len_v = q.size(0), q.size(1), k.size(1), v.size(1)
+        c, h, w = q.size(2), q.size(3), q.size(4)
+        assert len_k == len_v and c == d_feature
 
-        if mask is not None:
-            attn = attn.masked_fill(mask == 0, -1e9)
+        q, k, v = q.transpose(0, 1), k.transpose(0, 1), v.transpose(0, 1)
 
-        attn = self.dropout(F.softmax(attn, dim=-1))
-        output = torch.matmul(attn, v)
+        # Pass through the pre-attention projection: l x b x c x h x w
+        q = torch.stack([self.conv_qs(x) for x in q]).view(len_q, sz_b, d_model, h, w)
+        k = torch.stack([self.conv_ks(x) for x in k]).view(len_k, sz_b, d_model, h, w)
+        v = torch.stack([self.conv_vs(x) for x in v]).view(len_v, sz_b, d_model, h, w)
 
-        return output, attn
+        output = []
+        attentions = []
+        for i in len_q:
+            qk = torch.stack([q[i, ...]] * len_k) + k
+            attn = torch.stack([self.conv_attenion(x) for x in qk])
+            attn = F.softmax(attn, dim=0)
+            attn = self.dropout(attn)
+
+            res = torch.mul(attn, v).sum(dim=0)
+            output.append(res)
+            attentions.append(attn.transpose(0, 1))
+
+        output = torch.stack(output).transpose(0, 1)
+        attentions = torch.stack(attentions).transpose(0, 1)
+
+        # output size: b x lq x c x h x w
+        # attentions size: b x lq x lk x c x h x w
+        return output, attentions
--- conv_transofrmer/SubLayers.py
+++ conv_transofrmer/SubLayers.py
@@ -1,51 +1,59 @@
 ''' Define the sublayers in encoder/decoder layer '''
 import numpy as np
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from transformer.Modules import ScaledDotProductAttention
+from conv_transofrmer.Modules import ConvolutionalAttention
 
 
-class MultiHeadAttention(nn.Module):
+class MultiHeadConvAttention(nn.Module):
     ''' Multi-Head Attention module '''
 
-    def __init__(self, n_head, d_feature=32, d_model=32, dropout=0.1):
+    def __init__(self, n_head, h, w, d_feature=32, d_model=32, d_attention=1, dropout=0.1):
         super().__init__()
 
         self.n_head = n_head
+        self.d_feature = d_feature
+        self.d_model = d_model
+        self.d_attention = d_attention
+        self.h = h
+        self.w = w
 
-        self.conv_qs = nn.Conv2d(d_feature, n_head * d_model, kernel_size=1)
-        self.conv_ks = nn.Conv2d(d_feature, n_head * d_model, kernel_size=1)
-        self.conv_vs = nn.Conv2d(d_feature, n_head * d_model, kernel_size=1)
+        self.conv_attentions = [ConvolutionalAttention(d_feature, d_model, d_attention) for _ in n_head]
+        self.fc = nn.Linear(n_head * d_model * h * w, d_model * h * w, bias=False)
 
         self.dropout = nn.Dropout(dropout)
         self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
 
-    def forward(self, f_q, f_k, f_v, mask=None):
-        # input feature maps size: b, lq, c, h, w
+    def forward(self, q, k, v):
+        # input feature maps size: b, l, c, h, w
         n_head = self.n_head
-        sz_b, len_q, len_k, len_v = f_q.size(0), f_q.size(1), f_k.size(1), f_v.size(1)
-        c, h, w = f_q.size(2), f_q.size(3), f_q.size(4)
+        d_model = self.d_model
 
-        residual = f_q
+        sz_b, len_q, len_k, len_v = q.size(0), q.size(1), k.size(1), v.size(1)
+        c, h, w = q.size(2), q.size(3), q.size(4)
+        assert self.h == h and self.w == w
 
-        # Pass through the pre-attention projection: b x lq x n x c x h x w
-        # Separate different heads: b x lq x n x dv
-        q = self.w_qs(f_q).view(sz_b, len_q, n_head, c, h, w)
-        k = self.w_ks(f_k).view(sz_b, len_k, n_head, c, h, w)
-        v = self.w_vs(f_v).view(sz_b, len_v, n_head, c, h, w)
+        residual = q
+        multihead_q = []
+        multihead_attn = []
+        for i in n_head:
+            output, attn = self.conv_attentions[i](q, k, v)
+            multihead_q.append(output)
+            multihead_attn.append(attn)
 
-        # Transpose for attention dot product: b x n x lq x c x h x w
-        q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
-
-        q, attn = self.attention(q, k, v, mask=mask)
+        # size of q: n x b x lq x c x h x w
+        # size of attn: n x b x lq x lk x c x h x w
+        q, attn = torch.stack(multihead_q), torch.stack(multihead_attn)
 
         # Transpose to move the head dimension back: b x lq x n x dv
         # Combine the last two dimensions to concatenate all the heads together: b x lq x (n*dv)
-        q = q.transpose(1, 2).contiguous().view(sz_b, len_q, -1)
-        q = self.dropout(self.fc(q))
-        q += residual
-
+        q = q.transpose(0, 1).transpose(1, 2).contiguous().view(sz_b, len_q, -1)
+        q = self.fc(q)
         q = self.layer_norm(q)
+        q = q.view(sz_b, len_q, d_model, h, w)
+        q = self.dropout(q)
+        q += residual
 
         return q, attn
 
