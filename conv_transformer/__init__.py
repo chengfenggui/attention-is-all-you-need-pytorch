7d2449471cdf21256f657a0a3b8328318a83a786
zhongyuan <zhongyuan12345@qq.com>
2021-01-25 16:12:34 +0800

    fix init

--- conv_transformer/__init__.py
+++ conv_transformer/__init__.py
@@ -1,11 +1,7 @@
-import conv_transformer.Modules
-import conv_transformer.Layers
-import conv_transformer.SubLayers
-import conv_transformer.Models
-import conv_transformer.Translator
-import conv_transformer.Optim
+import conv_transformer.Modules as Modules
+import conv_transformer.Layers as Layers
+import conv_transformer.SubLayers as SubLayers
+import conv_transformer.Models as Models
+import conv_transformer.Optim as Optim
 
-__all__ = [
-    conv_transformer.Modules, conv_transformer.Layers,
-    conv_transformer.SubLayers, conv_transformer.Models,
-    conv_transformer.Optim, conv_transformer.Translator]
+__all__ = [Modules, Layers, SubLayers, Models, Optim]
